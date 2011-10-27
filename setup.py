e8fd8710ab8642f4e30b7d7f7418ed3533c0391d
Zakir Durumeric <zakird@gmail.com>
2011-10-27 13:25:20 -0400

    updating setup.py for correct upload to pypi

--- setup.py
+++ setup.py
@@ -1,18 +1,25 @@
 import os
 from setuptools import setup
 
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 setup(
     name = "pyad",
     version = "0.4.9",
     author = "Zakir Durumeric",
     author_email = "zakird@gmail.com",
-    description = ("A pure Python Object-Oriented Active Directory management framework built on ADSI through pywin32."),
+    maintainer = "Zakir Durumeric",
+    maintainer_email = "zakird@gmail.com",
+    url = "https://github.com/zakird/pyad",
+    description = "A pure Python Object-Oriented Active Directory management framework built on ADSI through pywin32.",
     license = "GNUv3",
     keywords = "python microsoft windows active directory AD adsi",
     packages=[
         'pyad'
     ],
-    long_description = "A pure Python Object-Oriented Active Directory management framework built on ADSI through pywin32.",
+    long_description = read('README'),
     classifiers=[
         "Development Status :: 4 - Beta",
         "License :: OSI Approved :: BSD License",
