2c5b6b8cfe75a7813477d87d372a630c3245984f
Zakir Durumeric <zakird@gmail.com>
2012-05-25 03:34:17 -0400

    bumping egg version for bugfix

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 
 setup(
     name = "pyad",
-    version = "0.4.11",
+    version = "0.4.12",
     author = "Zakir Durumeric",
     author_email = "zakird@gmail.com",
     maintainer = "Zakir Durumeric",
