778e1803f48cdbf6bbc8284533dec294c6548e59
AlexKavanaugh <kavanaugh.development@outlook.com>
2013-06-27 20:11:24 -0700

    Update adbase.py
    Added nifhell's LDAPS and Credential support.
    
    https://github.com/nifhell/pyad/commit/c27937c17e9a14209b4df884a090c1e9c92721c5

--- pyad/adbase.py
+++ pyad/adbase.py
@@ -14,22 +14,23 @@ try:
     import win32com.client
 except ImportError:
     raise Exception("pywin32 library required. Download from http://sourceforge.net/projects/pywin32/")
-    
+
 # Import constants and other common elements.
-from pyadconstants import *
-from pyadexceptions import *
+from .pyadconstants import *
+from .pyadexceptions import *
 
 # create connection to ADSI COM object
 _adsi_provider = win32com.client.Dispatch('ADsNameSpaces')
 
 # Discover default domain and forest information
-__default_domain_obj = _adsi_provider.GetObject('',"LDAP://rootDSE")
+__default_domain_obj = _adsi_provider.GetObject('', "LDAP://rootDSE")
 
-# connecting to rootDSE will connect to the domain that the 
-# current logged-in user belongs to.. which is generally the 
-# domain under question and therefore becomes the default domain. 
+# connecting to rootDSE will connect to the domain that the
+# current logged-in user belongs to.. which is generally the
+# domain under question and therefore becomes the default domain.
 _default_detected_forest = __default_domain_obj.Get("rootDomainNamingContext")
 _default_detected_domain = __default_domain_obj.Get("defaultNamingContext")
+
 
 class ADBase(object):
     """Base class that is utilized by all objects within package to help
@@ -38,11 +39,16 @@ class ADBase(object):
     default_gc_server = None
     default_ldap_port = None
     default_gc_port = None
+    default_ldap_protocol = 'LDAP'
+    default_ldap_usn = None
+    default_ldap_pwd = None
+    default_ldap_authentication_flag = 0  # No credentials
     default_domain = _default_detected_domain
     default_forest = _default_detected_forest
     adsi_provider = _adsi_provider
 
+
 def set_defaults(**kwargs):
     for k, v in kwargs.iteritems():
-        assert k in ('ldap_server','gc_server','ldap_port','gc_port')
-        setattr(ADBase, '_'.join(('default',k)), v)
+        assert k in ('ldap_server', 'gc_server', 'ldap_port', 'gc_port', 'ldap_protocol', 'ldap_authentication_flag', 'ldap_usn', 'ldap_pwd')
+        setattr(ADBase, '_'.join(('default', k)), v)
