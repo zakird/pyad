2e3c417ab18d5e1e51bd770a56e9261d70b2e483
Vince-AC <vincentmerlino@gmail.com>
2016-01-14 13:14:58 -0500

    Update adcomputer.py

--- pyad/adcomputer.py
+++ pyad/adcomputer.py
@@ -1,4 +1,6 @@
-from adobject import *
+from __future__ import absolute_import
+from builtins import str
+from .adobject import *
 
 class ADComputer(ADObject):
     """Python class representing a computer object in Active Directory."""
