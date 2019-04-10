33eac5b1b52fabcb787a91ae27d128f9a45176af
Maku <makusu2@gmail.com>
2019-04-10 11:44:32 -0400

    Added credential validation, fixed trailing whitespace

--- pyad/pyadutils.py
+++ pyad/pyadutils.py
@@ -6,17 +6,30 @@ from builtins import str
 from .adbase import *
 from datetime import timedelta
 
+def validate_credentials(username, password, domain=None):
+    """Validates credentaials; returns a PyHANDLE object with a bool value
+    of True if the credentials are valid, else returns None.
+    """
+    try:
+        valid = win32security.LogonUser(username, domain, password,
+                                        win32security.LOGON32_LOGON_NETWORK,
+                                        win32security.LOGON32_PROVIDER_DEFAULT)
+        assert valid, "Valid should ALWAYS have a true value"
+        return valid
+    except pywintypes.error:
+        return None
+
 def convert_error_code(error_code):
     """Convert error code from the format returned by pywin32 to the format that Microsoft documents everything in."""
     return error_code % 2 ** 32
 
 #expects the actualy pywintypes.com_error exception that's thrown...
-def interpret_com_exception(excp, additional_info={}):  
+def interpret_com_exception(excp, additional_info={}):
     d = {}
     d['error_num'] = convert_error_code(excp.args[2][5])
     # for some reason hex() includes the L for long in the hex...
-    # however since it's a string, we don't care... 
-    # since L would never be in a hex code, we can safely just remove it. 
+    # however since it's a string, we don't care...
+    # since L would never be in a hex code, we can safely just remove it.
     d['error_code'] = hex(d['error_num']).rstrip('L')
     if d['error_code'][0:7] == '0x80005':
         if d['error_num'] in list(GENERIC_ADSI_ERRORS.keys()):
@@ -25,7 +38,7 @@ def interpret_com_exception(excp, additional_info={}):
             d['message'] = ' '.join(GENERIC_ADSI_ERRORS[d['error_num']][1:3])
         else:
             # this supposedly should not happen, but I'd rather be ready for
-            # the case that Microsoft made a typo somewhere than die weirdly. 
+            # the case that Microsoft made a typo somewhere than die weirdly.
             d['error_constant'] = None
             d['exception_type'] = 'unknown_generic_adsi_error'
             d['message'] = 'unknown generic ADSI error'
@@ -33,8 +46,8 @@ def interpret_com_exception(excp, additional_info={}):
     elif d['error_code'][0:6] == '0x8007':
         d['exception_type'] = 'win32_error'
         d['error_constant'] = None
-        # returns information about error from winerror.h file... 
-        d['message'] = win32api.FormatMessage(d['error_num']) 
+        # returns information about error from winerror.h file...
+        d['message'] = win32api.FormatMessage(d['error_num'])
     elif d['error_num'] in list(GENERIC_COM_ERRORS.keys()):
         d['exception_type'] = 'generic_com_error'
         d['error_constant'] = GENERIC_COM_ERRORS[d['error_num']][0]
@@ -66,7 +79,7 @@ def pass_up_com_exception(excp, additional_info={}):
 def convert_datetime(adsi_time_com_obj):
     """Converts 64-bit integer COM object representing time into a python datetime object."""
     # credit goes to John Nielsen who documented this at
-    # http://docs.activestate.com/activepython/2.6/pywin32/html/com/help/active_directory.html. 
+    # http://docs.activestate.com/activepython/2.6/pywin32/html/com/help/active_directory.html.
 
     high_part = int(adsi_time_com_obj.highpart) << 32
     low_part = int(adsi_time_com_obj.lowpart)
@@ -80,14 +93,14 @@ def convert_datetime(adsi_time_com_obj):
     if date_value < 0:
         date_value = 18000
     return datetime.datetime.fromtimestamp(date_value)
-        
+
 def convert_bigint(obj):
     # based on http://www.selfadsi.org/ads-attributes/user-usnChanged.htm
     h, l = obj.HighPart, obj.LowPart
     if l < 0:
         h += 1
     return (h << 32) + l
-    
+
 def convert_timespan(obj):
     """Converts COM object representing timespan to a python timespan object."""
     as_seconds = abs(convert_bigint(obj))/10000000 #number of 100 nanoseconds in a second
@@ -98,7 +111,7 @@ def convert_guid(guid_object):
 
 def convert_sid(sid_object):
     return pywintypes.SID(bytes(sid_object))
-    
+
 def generate_list(input):
     if type(input) is list:
         return input
@@ -106,7 +119,7 @@ def generate_list(input):
         return list(input)
     else:
         return [input,]
-        
+
 def escape_path(path):
     escapes = (
         ('\+','+'),
@@ -130,13 +143,13 @@ def escape_path(path):
 
 def generate_ads_path(distinguished_name, type_, server=None, port=None):
     """Generates a proper ADsPath to be used when connecting to an active directory object or when searching active directory.
-    
+
     Keyword arguments:
      - distinguished_name: DN of object or search base such as 'cn=zakir,ou=users,dc=mycompany,dc=com' (required).
      - type: 'GC' (global-catalog) or 'LDAP' to determine what directory to be searched (required).
      - server: FQDN of domain controller if necessary to connect to a particular server (optional unless port is defined).
      - port: port number for directory service if not default port. If port is specified, server must be specified (optional)."""
-    
+
     if type_ == "LDAP"  or type_ == "LDAPS":
         server = server if server else ADBase.default_ldap_server
         port = port if port else ADBase.default_ldap_port
@@ -145,7 +158,7 @@ def generate_ads_path(distinguished_name, type_, server=None, port=None):
         port = port if port else ADBase.default_gc_port
     else:
         raise Exception("Invalid type specified.")
-        
+
     ads_path = ''.join((type_,'://'))
     if server:
         ads_path = ''.join((ads_path,server))
